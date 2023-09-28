36a078d0beb1971b66e3310bdb47203b3c8444a3
Nabila Fakhirah <nabilafakhirah@gmail.com>
2023-09-29 01:33:58 +0700

    Set up fragments

--- app/src/main/java/com/example/marvelmovies/MainActivity.kt
+++ app/src/main/java/com/example/marvelmovies/MainActivity.kt
@@ -1,61 +1,40 @@
 package com.example.marvelmovies
 
 import android.os.Bundle
-import android.util.Log
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.ViewModelProvider
-import com.example.marvelmovies.data.Search
-import com.example.marvelmovies.ui.theme.MarvelMoviesTheme
-import com.example.marvelmovies.ui.theme.Typography
-import com.example.marvelmovies.viewmodel.AvengerMoviesViewModel
-import com.example.marvelmovies.viewmodel.AvengerMoviesViewModelFactory
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MarvelMoviesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    val viewModel = ViewModelProvider(this,
-                        AvengerMoviesViewModelFactory())[AvengerMoviesViewModel::class.java]
-                    Greeting(
-                        name = "Android",
-                        viewModel = viewModel,
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(
-    name: String,
-    viewModel: AvengerMoviesViewModel,
-    modifier: Modifier = Modifier,) {
-    val searchList by remember { viewModel.searchList }
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-    searchList.forEach {
-        Text(
-            text = it.title,
-            style = Typography.headlineLarge
-        )
+        val homeFragment = HomeFragment()
+        val downloadsFragment = DownloadsFragment()
+        val categoriesFragment = CategoriesFragment()
+        val moreFragment = MoreFragment()
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home_menu -> setCurrentFragment(homeFragment)
+                R.id.downloads_menu -> setCurrentFragment(downloadsFragment)
+                R.id.categories_menu -> setCurrentFragment(categoriesFragment)
+                R.id.more_menu -> setCurrentFragment(moreFragment)
+
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
 }
